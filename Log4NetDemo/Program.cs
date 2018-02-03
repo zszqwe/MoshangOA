458d480892899cc0c060c7496e04072e83ca29cf
Moshang <l_moshang@foxmail.com>
2018-02-03 21:10:36 +0800

    Log4Net实例测试

--- Log4NetDemo/Program.cs
+++ Log4NetDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace Log4NetDemo
 {
@@ -10,6 +11,17 @@ namespace Log4NetDemo
     {
         static void Main(string[] args)
         {
+            log4net.Config.XmlConfigurator.Configure();
+
+            //记录日志
+            ILog logWrite = log4net.LogManager.GetLogger("Log4NetDemoWrite");
+
+            /*
+            logWrite.Debug("Debug级别");//调试
+            logWrite.Error("Error级别");//异常
+            */
+
+
         }
     }
 }
