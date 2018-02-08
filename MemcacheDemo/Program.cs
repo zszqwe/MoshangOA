25e4d1d7bb0fd9d1c90111297c3e61aed80cfd6e
Moshang <l_moshang@foxmail.com>
2018-02-08 23:14:11 +0800

    Memcache 测试实例

---
+++ MemcacheDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Memcached.ClientLibrary;
+
+namespace MemcacheDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //分布Memcachedf服务IP 端口
+            string[] servers =
+            {
+                "192.168.1.105:11211"
+            };
+
+            //初始化池
+            SockIOPool pool = SockIOPool.GetInstance();
+            pool.SetServers(servers);
+            pool.InitConnections = 3;
+            pool.MinConnections = 3;
+            pool.MaxConnections = 5;
+            pool.SocketConnectTimeout = 1000;
+            pool.SocketTimeout = 3000;
+            pool.MaintenanceSleep = 30;
+            pool.Failover = true;
+            pool.Nagle = false;
+            pool.Initialize();
+            //客户端实例
+            MemcachedClient mc = new Memcached.ClientLibrary.MemcachedClient();
+            mc.EnableCompression = false;
+
+
+            mc.Add("key1", "value1");
+        }
+    }
+}
